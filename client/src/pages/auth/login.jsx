25909837021c07646a9368ebd6c46da5764ff6c9
Vladyslav Plugin <kainec.it@gmail.com>
2025-02-11 15:55:30 +0100

    login done for now

--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 //import axios from "axios";
 
 function AuthLogin() {
@@ -15,10 +15,10 @@ function AuthLogin() {
     //    .catch((err) => console.log(err));
 
     return (
-      <div className="border-b border-gray-900/10 pb-12 ">
-          <h2 className="text-base/7 font-semibold text-gray-900">Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <h2 className="sm:mx-auto sm:w-full sm:max-w-sm">Login</h2>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <label className="block text-sm/6 font-medium text-gray-900" htmlFor="email">email</label>
               <input
                 value={email}
@@ -30,7 +30,7 @@ function AuthLogin() {
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <label className="block text-sm/6 font-medium text-gray-900" htmlFor="password">password</label>
               <input
                 value={pass}
@@ -42,14 +42,14 @@ function AuthLogin() {
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
-            <div className="mt-6 flex items-center justify-start gap-x-6">
-            <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">Login</button>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">Login</button>
             </div>
           </form>
-          <p>Dont have an account?</p>
-          <Link to="/register" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <p className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">Dont have an account?  <a href="/register" className="underline decoration-sky-500">
             Register
-          </Link>
+          </a></p>
+         
         </div>
     );
   };
--- client/src/pages/auth/register.jsx
+++ client/src/pages/auth/register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 //import axios from "axios";
 
 function AuthRegister() {
@@ -15,10 +15,10 @@ function AuthRegister() {
     //    .catch((err) => console.log(err));
 
     return (
-      <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base/7 font-semibold text-gray-900">Register</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-[#6C6A61]">
+          <h2 className="sm:mx-auto sm:w-full sm:max-w-sm">Register</h2>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <label
                 className="block text-sm/6 font-medium text-gray-900"
                 htmlFor="name"
@@ -34,7 +34,7 @@ function AuthRegister() {
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <label className="block text-sm/6 font-medium text-gray-900" htmlFor="email">email</label>
               <input
                 value={email}
@@ -46,7 +46,7 @@ function AuthRegister() {
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <label className="block text-sm/6 font-medium text-gray-900" htmlFor="password">password</label>
               <input
                 value={pass}
@@ -58,14 +58,14 @@ function AuthRegister() {
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
               />
             </div>
-            <div className="mt-6 flex items-center justify-start gap-x-6">
-            <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">Register</button>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="submit">Register</button>
             </div>
           </form>
-          <p>Already have an account?</p>
-          <Link to="/login" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <p className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">Already have an account? <a href="/login" className="underline decoration-sky-500" >
             Login
-          </Link>
+          </a> </p>
+          
         </div>
     );
   };
