2b756a9465faf3f130e4f115e0d6db51e6e67ce7
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2025-03-01 12:33:18 +0100

    fixing some bugs

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,10 +2,8 @@ import express, { Router } from 'express';
 
 import { signup, login, logout , verifyEmail, forgotPassword, resetPassword, checkAuth} from '../controllers/authController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
-
-
-import { signup, login, logout , verifyEmail, forgotPassword, resetPassword} from '../controllers/authController.js';
 import { verify } from 'crypto';
+import crypto from 'crypto';
 
 
 const router = express.Router();
