056fe0812c59aee0267682bd6ee9a7faa703645e
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2025-03-05 13:00:45 +0100

    addded an onClick event on the Home page images so they can redirect the user to the relevant page onClick

--- client/src/components/shopping-view/Background.jsx
+++ client/src/components/shopping-view/Background.jsx
@@ -6,9 +6,11 @@ import video2 from "../../assets/Background/men video.mp4";
 import womenItemsImg from "../../assets/Background/women items.jpg";
 import menItemsImg from "../../assets/Background/men items.jpg";
 import accessoiresImg from "../../assets/Background/accessoires items .jpg";
+import {useNavigate} from "react-router-dom";
 
 
 const Background = () => {
+  const navigate = useNavigate();
   return (
     <div className="hero-section">
       <Swiper
@@ -34,21 +36,29 @@ const Background = () => {
 
         <SwiperSlide>
           <div className="slide-content item-container">
+          <button onClick={() => navigate('/category/ladies')} className="item-button">
             <img
               src={womenItemsImg}
               alt="Women's Collection"
               className="item-image women-image"
             />
+            </button>
+            <button onClick={() => navigate('/category/accessories')} className="item-button">
             <img
               src={accessoiresImg}
               alt="Accessories Collection"
               className="item-image accessories-image"
             />
+            </button>
+            <button onClick={() => navigate('/category/men')} className="item-button">
+            shop now
             <img
               src={menItemsImg}
               alt="Men's Collection"
               className="item-image"
+              
             />
+            </button>
           </div>
         </SwiperSlide>
       </Swiper>
